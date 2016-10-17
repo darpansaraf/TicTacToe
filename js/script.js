d43ff8b95824597eade66809add62dba512e9044
dsaraf@tavisca.com <dsaraf@tavisca.com>
2016-10-17 22:14:04 +0530

    Added Implementation to display match tied.Added cancel button to winning details modal.

--- js/script.js
+++ js/script.js
@@ -5,13 +5,17 @@ var board = [
   [-1,-1,-1],
   [-1,-1,-1]
 ];
+var matchTied = true;
+var counter = 0;
 
 function showWinner(winnerId)
 {
   if(winnerId === 1)
   	document.getElementById("winnerDetails").innerHTML = "Player 1 Wins..!";
-  else
+  else if(winnerId === 2)
     document.getElementById("winnerDetails").innerHTML = "Player 2 Wins..!";
+  else
+    document.getElementById("winnerDetails").innerHTML = "Match Tied..!"
   // Get the modal
 var modal = document.getElementById('myModal');
 
@@ -21,6 +25,7 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+    window.location.reload();
 };
 
 modal.style.display = "block";
@@ -33,55 +38,77 @@ window.onclick = function(event) {
 };
 }
 
+
+
 function CheckBoard(){
+  counter++;
   if(board[0][0] === 0 && board[0][1] === 0 && board[0][2] === 0){
+  matchTied = false;
 	showWinner(1);    
   }
   else if(board[0][0] === 1 && board[0][1] === 1 && board[0][2] === 1){
+    matchTied = false;
     showWinner(2);
   }
   else if(board[1][0] === 0 && board[1][1] === 0 && board[1][2] === 0){
+    matchTied = false;
     showWinner(1);
   }
   else if(board[1][0] === 1 && board[1][1] === 1 && board[1][2] === 1){
+    matchTied = false;
     showWinner(2);
   }
   else if(board[2][0] === 0 && board[2][1] === 0 && board[2][2] === 0){
+    matchTied = false;
     showWinner(1);
   }
   else if(board[2][0] === 1 && board[2][1] === 1 && board[2][2] === 1){
+    matchTied = false;
     showWinner(2);
   }
   else if(board[0][0] === 0 && board[1][0] === 0 && board[2][0] === 0){
-showWinner(1);    
+    matchTied = false;
+    showWinner(1);    
   }
   else if(board[0][0] === 1 && board[1][0] === 1 && board[2][0] === 1){
+    matchTied = false;
     showWinner(2);
   }
   else if(board[0][1] === 0 && board[1][1] === 0 && board[2][1] === 0){
+    matchTied = false;
     showWinner(1);
   }
   else if(board[0][1] === 1 && board[1][1] === 1 && board[2][1] === 1){
+    matchTied = false;
     showWinner(2);
   }
   else if(board[0][2] === 0 && board[1][2] === 0 && board[2][2] === 0){
+    matchTied = false;
     showWinner(1);
   }
   else if(board[0][2] === 1 && board[1][2] === 1 && board[2][2] === 1){
+    matchTied = false;
     showWinner(2);
   }
   else if(board[0][0] === 0 && board[1][1] === 0 && board[2][2] === 0){
+    matchTied = false;
     showWinner(1);
   }
   else if(board[0][0] === 1 && board[1][1] === 1 && board[2][2] === 1){
+    matchTied = false;
     showWinner(2);
   }
   else if(board[2][0] === 0 && board[1][1] === 0 && board[0][2] === 0){
+    matchTied = false;
     showWinner(1);
   }
   else if(board[2][0] === 1 && board[1][1] === 1 && board[0][2] === 1){
+    matchTied = false;
     showWinner(2);
   }
+
+  if(counter === 9 && matchTied === true)
+    showWinner(-1);
 }
 
  function animateCircle(current) {
@@ -126,7 +153,7 @@ context.shadowColor = '#656565';
 x = canvas.width / 2;
 y = canvas.height / 2;
 radius = 25;
-endPercent = 102;
+endPercent = 110;
 curPerc = 0;
 counterClockwise = true;
 circ = Math.PI * 2;
